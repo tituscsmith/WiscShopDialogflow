4d1b1026fe69a98b15c981f36df9aed58cf5bea8
tituscsmith <tsmith42@wisc.edu>
2020-12-28 00:18:13 -0500

    Updates

---
+++ precache-manifest.1f0e83c18c2c9886ad65ee5ece035cef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43052b70a270eff7f9ffe7b9ddbc93da",
+    "url": "/tituscsmith/WiscShopDialogflow/index.html"
+  },
+  {
+    "revision": "b8921e9353b8c14e1c3c",
+    "url": "/tituscsmith/WiscShopDialogflow/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "6c40b9f1bb17250dea5a",
+    "url": "/tituscsmith/WiscShopDialogflow/static/css/main.40f2b289.chunk.css"
+  },
+  {
+    "revision": "b8921e9353b8c14e1c3c",
+    "url": "/tituscsmith/WiscShopDialogflow/static/js/2.1824ffa6.chunk.js"
+  },
+  {
+    "revision": "6c40b9f1bb17250dea5a",
+    "url": "/tituscsmith/WiscShopDialogflow/static/js/main.8608f12d.chunk.js"
+  },
+  {
+    "revision": "90ed8255eae9b24bf407",
+    "url": "/tituscsmith/WiscShopDialogflow/static/js/runtime-main.deefa3e4.js"
+  }
+]);
